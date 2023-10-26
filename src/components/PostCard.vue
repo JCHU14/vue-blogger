8031dc75ef13e200e5dca6e1224622db532e040d
Cody <qtipcody@gmail.com>
2023-10-26 15:31:08 -0600

    Has a Profile page

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,25 +1,32 @@
 <template>
     <div class="text-dark text-center m-5">
         <img class="imageBlock borderBox" :src="postProp.imgUrl" alt="">
-        <div>
-            <img class="creatorPic m-4 rounded-circle" :src="postProp.creator.picture" alt="">
-
-        </div>
+        <router-link :to="{ name: 'Profile', params: { profileId: postProp.creator.id } }">
+            <div>
+                <img class="creatorPic m-4 rounded-circle" :src="postProp.creator.picture" alt="">
+            </div>
+        </router-link>
         <p class="fs-2">{{ postProp.title }}</p>
         <p class="fs-5">{{ postProp.body }}</p>
+
     </div>
 </template>
 
 
 <script>
+import { AppState } from '../AppState.js';
 import { Post } from '../models/Post';
-
+import { computed } from 'vue'
 export default {
     props: {
         postProp: { type: Post, required: true },
     },
     setup() {
         return {
+            account: computed(() => AppState.account)
+
+
+
 
         }
     }
