dc8efa6be16ceaf14c869bec5745e7900f48b2e6
Jaret Hunemiller <jchunemiller@gmail.com>
2023-10-26 14:58:41 -0600

    com

---
+++ src/components/PostCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="text-dark text-center m-5">
+        <img class="imageBlock borderBox" :src="postProp.imgUrl" alt="">
+        <div>
+            <img class="creatorPic m-4 rounded-circle" :src="postProp.creator.picture" alt="">
+
+        </div>
+        <p class="fs-2">{{ postProp.title }}</p>
+        <p class="fs-5">{{ postProp.body }}</p>
+    </div>
+</template>
+
+
+<script>
+import { Post } from '../models/Post';
+
+export default {
+    props: {
+        postProp: { type: Post, required: true },
+    },
+    setup() {
+        return {
+
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.imageBlock {
+    width: 35vh;
+    height: 35vh;
+    object-fit: cover;
+}
+
+.creatorPic {
+    width: 10vh;
+    height: 10vh;
+}
+
+.borderBox {
+    border: 5px solid black;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,17 +4,7 @@
     <div class="row justify-content-center">
 
       <div v-for="post in posts" :key="post.id" class="col-7 bg-warning m-5 borderBox">
-
-        <div class="text-dark text-center m-5">
-          <img class="imageBlock borderBox" :src="post.imgUrl" alt="">
-          <div>
-            <img class="creatorPic m-4 rounded-circle" :src="post.creator.picture" alt="">
-
-          </div>
-          <p class="fs-2">{{ post.title }}</p>
-          <p class="fs-5">{{ post.body }}</p>
-        </div>
-
+        <PostCard :postProp="post" />
       </div>
 
     </div>
@@ -27,46 +17,31 @@ import { computed, onMounted } from 'vue';
 import { postsService } from '../services/PostsService.js';
 import Pop from '../utils/Pop';
 import { AppState } from '../AppState.js';
+import PostCard from '../components/PostCard.vue';
 
 
 export default {
-
   setup() {
     async function getPosts() {
       try {
         await postsService.getPosts();
-      } catch (error) {
-        Pop.error(error)
+      }
+      catch (error) {
+        Pop.error(error);
       }
     }
     onMounted(() => {
-
       getPosts();
-
-    })
+    });
     return {
       posts: computed(() => AppState.posts),
       account: computed(() => AppState.account)
-    }
-  }
+    };
+  },
+  components: { PostCard }
 }
 </script>
 
 
 
-<style scoped lang="scss">
-.imageBlock {
-  width: 35vh;
-  height: 35vh;
-  object-fit: cover;
-}
-
-.creatorPic {
-  width: 10vh;
-  height: 10vh;
-}
-
-.borderBox {
-  border: 5px solid black;
-}
-</style>
+<style scoped lang="scss"></style>
